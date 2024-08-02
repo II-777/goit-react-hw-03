9cb2e916d676165ac7aaed5e17052fd069119362
II-777 <45958058+II-777@users.noreply.github.com>
2024-08-02 00:59:41 -0400

    add step 2

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,12 +2,13 @@ import css from './SearchBox.module.css';
 
 export default function Filter({ value, onFilter }) {
   return (
-    <div>
+    <div className={css.SearchBox}>
       <p className={css.label}>Search contacts by name</p>
       <input
         type="text"
         value={value}
         onChange={(e) => onFilter(e.target.value)}
+        className={css.input} // Add this line to apply the input styles
       />
     </div>
   );
