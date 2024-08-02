b1ae3dd21a42d2d67c2cc7a0d2e9cab1b9b52b07
II-777 <45958058+II-777@users.noreply.github.com>
2024-08-01 23:51:16 -0400

    add step 1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import ContactList from '../ContactList/ContactList';
+import initialContacts from '../../data/contacts.json';
+import css from './App.module.css';
+import { useState } from 'react';
+import { ImAddressBook } from "react-icons/im";
+
+export default function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div className={css.container}>
+      <h1><ImAddressBook className={css.icon} /> Phonebook</h1>
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+    </div>
+  );
+}
